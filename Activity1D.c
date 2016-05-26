198a842f2c3839b02b46ef780cec7f72c2e0fc1b
MharelaAngela <mhargelatan@gmail.com>
2016-05-26 17:29:12 +0800

    Create Activity1D.c

---
+++ Activity1D.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+                                        
+int main()
+{
+	float x, ans;
+	printf("Enter value of X:  ");
+	scanf("%f", &x);
+	ans= x/70;
+	printf("Value of f(x) = %f\n\n", ans);
+	
+	system ("PAUSE");
+}
