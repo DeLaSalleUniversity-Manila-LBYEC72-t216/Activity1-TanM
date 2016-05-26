de0b2f89a5627bd6ee0c07ff8e24f04ad22a3dc1
MharelaAngela <mhargelatan@gmail.com>
2016-05-26 17:28:33 +0800

    Create Activity1C.c

---
+++ Activity1C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+                                        
+int main()
+{
+	int year, age;
+	printf("Enter birth year:  ");
+	scanf("%d", &year);
+	age = 2016-year;
+	
+	printf("\nAGE: %d\n\n", age);
+	system ("PAUSE");
+}
