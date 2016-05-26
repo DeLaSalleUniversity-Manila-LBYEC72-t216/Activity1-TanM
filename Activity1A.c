975376763caa2c31e4503f4d4bbb6f8b329ff2f3
MharelaAngela <mhargelatan@gmail.com>
2016-05-26 17:27:19 +0800

    Create Activity1A.c

---
+++ Activity1A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+                                        
+int main()
+{
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	printf("\n         Mharela Angela A. Tan      \n                BS ECE ");
+	printf("\n                ID NUMBER           \n              May 26, 2016");
+	printf("\n           e-mail address            \n                MOBILE NUMBER");
+	printf("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+	
+	system ("PAUSE");
+}
+	
